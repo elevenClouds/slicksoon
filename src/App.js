7f514356d58066d3488c09e1f6e773400c717b8a
eleven Clouds <reach@elevens.cloud>
2018-10-10 19:34:07 +0100

    Add semantic-ui library with new logo component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Logo from './Components/Logo';
+
+
+import './semantic/dist/semantic.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Logo></Logo>
       </div>
     );
   }
---
+++ src/Components/Logo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+
+class Logo extends Component {
+  render() {
+    return (
+      <div className="Logo">
+        <Button>Click Here</Button>
+      </div>
+    );
+  }
+}
+
+export default Logo;
